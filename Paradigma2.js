3e4145e3e6b6fe335112cbbda46b72872bf5790e
vbreiis <141686348+vbreiis@users.noreply.github.com>
2025-05-07 21:59:45 -0300

    Delete Paradigma2.js

--- Paradigma2.js
+++
@@ -1,8 +0,0 @@
-const criarTarefa = (titulo) => ({ titulo, concluida: false });
-
-const concluirTarefa = (tarefa) => ({ ...tarefa, concluida: true });
-
-const adicionarTarefa = (lista, tarefa) => [...lista, tarefa];
-
-const listarTarefas = (lista) =>
- lista.map((t) => `${t.titulo} - ${t.concluida ? 'Concluída' : 'Pendente'}`);
